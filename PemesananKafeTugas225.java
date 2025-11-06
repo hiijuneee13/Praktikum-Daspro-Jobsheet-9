10ccd3b973684043c76895ec7c931a4061738c57
Rizal Maulana <xjunngoat1@gmail.com>
2025-11-06 07:55:05 +0700

    Tugas 2

--- PemesananKafeTugas225.java
+++ PemesananKafeTugas225.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class PemesananKafeTugas125 {
+public class PemesananKafeTugas225 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan jumlah pesanan: ");
