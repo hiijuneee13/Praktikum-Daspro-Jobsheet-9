9d803164d257f64b976d4a04e1e7cc088db6f3d3
Rizal Maulana <xjunngoat1@gmail.com>
2025-11-06 07:46:36 +0700

    Modifikasi percobaan 2

--- ArrayNilai25.java
+++ ArrayNilai25.java
@@ -3,14 +3,22 @@ public class ArrayNilai25 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
-        System.out.println("Masukkan 10 nilai mahasiswa:");
+
+        System.out.println("Masukkan nilai akhir:");
         for (int i = 0; i < nilaiAkhir.length; i++) {
-            System.out.print("Nilai mahasiswa ke-" + (i+1) + ": ");
+            System.out.print("Masukkan nilai akhir ke-"+i+" : ");
             nilaiAkhir[i] = sc.nextInt();
         }
-        System.out.println("\nIsi array nilaiAkhir:");
+        System.out.println("\nNilai mahasiswa yang lulus (>70):");
         for (int i = 0; i < nilaiAkhir.length; i++) {
-            System.out.println("nilaiAkhir[" + i + "] = " + nilaiAkhir[i]);
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-"+i+" lulus!");
+            }
+        }
+        System.out.println("\nStatus kelulusan:");
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            String status = (nilaiAkhir[i] > 70) ? "LULUS!" : "TIDAK LULUS!";
+            System.out.println("Mahasiswa ke-"+i+" : " + nilaiAkhir[i] + " -> " + status);
         }
         sc.close();
     }
